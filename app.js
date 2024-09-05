379b35e3e9b367d66449d7a283b28954024bb644
shineni1031 <shineni1030@gmail.com>
2024-09-05 14:13:24 +0800

    build: add static files & json for model

--- app.js
+++ app.js
@@ -2,9 +2,9 @@ const express = require('express')
 const app = express()
 const port = 3000
 
-app.get('/', (req, res) => {
-  res.send('express app for movies')
-})
+app.use(express.static('public'))
+
+
 
 app.get('/', (req, res) => {
   res.redirect('/movies')
@@ -15,7 +15,7 @@ app.get('/movies', (req, res) => {
 })
 
 app.get('/movie/:id', (req, res) => {
-  const id = req class="params id"
+  const id = req.params.id
   res.send(`read movie: ${id}`)
 })
 
