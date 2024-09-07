49edf93b14996f46bf5d9a739e2013c50e7c4229
shineni1031 <shineni1030@gmail.com>
2024-09-07 08:31:52 +0800

    feat: add search function to listing

--- app.js
+++ app.js
@@ -16,7 +16,16 @@ app.get('/', (req, res) => {
 })
 
 app.get('/movies', (req, res) => {
-  res.render('index', { movies, BASE_IMG_URL })
+  const keyword = req.query.search?.trim()
+  const matchedMovies = keyword ? movies.filter((mv) =>
+    Object.values(mv).some((property) => {
+      if (typeof property === 'string') {
+        return property.toLowerCase().includes(keyword.toLowerCase())
+      }
+      return false
+    })
+  ) : movies
+  res.render('index', { movies: matchedMovies, BASE_IMG_URL, keyword })
 })
 
 app.get('/movie/:id', (req, res) => {
